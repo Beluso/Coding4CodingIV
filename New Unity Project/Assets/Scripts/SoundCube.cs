596120caf9c22141d4c1c67268b24705a4894ae7
unknown <student@AI251LAB0201P03.aicala.edmc.stu>
2015-02-06 19:42:57 -0800

    Added AudioManager scripts to implement in scene later. Modified the randomness of the mesh deformation a bit.

---
+++ New Unity Project/Assets/Scripts/SoundCube.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundCube : MonoBehaviour 
+{
+	public AudioClip explodeNoise;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log ("what" + Time.time);
+		Instantiate (Resources.Load ("explode"), transform.position, transform.rotation);
+		AudioManager.Instance.PlaySound (explodeNoise);
+	}
+}
