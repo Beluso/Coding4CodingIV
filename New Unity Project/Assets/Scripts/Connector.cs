63ea78e1408f6a6a397ca8d0ce6ac37df6df5f07
unknown <student@AI251LAB0201P14.aicala.edmc.stu>
2015-02-27 19:04:19 -0800

    Started networking functionality in scene Network.

---
+++ New Unity Project/Assets/Scripts/Connector.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Connector : MonoBehaviour {
+
+	public string connectionIP = "127.0.0.1";
+	public int connectionPort = 25001;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+//			PrintMsg("fart" + Time.time.ToString());
+			networkView.RPC ("PrintMsg", RPCMode.All, "fart" + Time.time.ToString());
+		}
+	}
+
+	[RPC]
+	void PrintMsg(string msg)
+	{
+		print (msg);
+	}
+	
+	//Network.OnPlayerConnected
+	void OnClientConnect()
+	{
+		//Network.Instantiate(cube)
+	}
+
+	void OnConnectedToServer()
+	{
+		//send MoTD, spawn characters, etc.
+		//Instantiate(cube)
+	}
+	
+	void OnGUI()
+	{
+		if (Network.peerType == NetworkPeerType.Disconnected)
+		{
+			GUI.Label(new Rect(10, 10, 300, 20), "Status: Disconnected");
+			if (GUI.Button(new Rect(10, 30, 120, 20), "Client Connect"))
+			{
+				Network.Connect(connectionIP, connectionPort);
+			}
+			if (GUI.Button(new Rect(10, 50, 120, 20), "Initialize Server"))
+			{
+				Network.InitializeServer(32, connectionPort, false);
+			}
+		}
+		else if (Network.peerType == NetworkPeerType.Client)
+		{
+			GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Client");
+			if (GUI.Button(new Rect(10, 30, 120, 20), "Disconnect"))
+			{
+				Network.Disconnect(200);
+			}
+		}
+		else if (Network.peerType == NetworkPeerType.Server)
+		{
+			GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Server");
+			if (GUI.Button(new Rect(10, 30, 120, 20), "Disconnect"))
+			{
+				Network.Disconnect(200);
+			}
+		}
+	}
+}
---
+++ New Unity Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour 
+{
+	public float speed = 10;
+	public Vector3 velocity;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//NetworkInstantiate
+		//NetworkView.owner
+		if (Network.isServer)
+		{
+			velocity = Vector3.zero;
+			if (Input.GetKey(KeyCode.W))
+			{
+				velocity = speed * transform.forward;
+				transform.position += speed * transform.forward * Time.deltaTime;
+			}
+			else if (Input.GetKey(KeyCode.S))
+			{
+				velocity = -speed * transform.forward;
+				transform.position -= speed * transform.forward * Time.deltaTime;
+			}
+			networkView.RPC ("SetTrajectory", RPCMode.Others, transform.position, velocity);
+		}
+		else
+		{
+			//client behavior
+			transform.position += velocity * Time.deltaTime;
+		}
+	}
+
+	[RPC]
+	void SetTrajectory(Vector3 pos, Vector3 v)
+	{
+		transform.position = pos;
+		velocity = v;
+	}
+}
